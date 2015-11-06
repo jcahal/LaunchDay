d1102fa0235ca137b03a43bccad120fbb0e141db
jcahal <jacahal@gmail.com>
2015-11-06 10:24:58 -0700

    Initial Commit

---
+++ LaunchDay.ino
@@ -0,0 +1,21 @@
+void setup() {
+  // put your setup code here, to run once:
+  int state = 0; //for launch day switch statement
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+  //Launch Day State Machine
+  switch(state) {
+    case: 0      //State 00, Waiting State
+      //some code here
+      break;
+
+    case: 1      //State 01, Data Collection State
+      //some code here
+      break;
+  }
+}
