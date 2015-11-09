5f4dfaac12d72dd39abaf5d521128eab67252e19
jcahal <jacahal@gmail.com>
2015-11-09 09:13:52 -0700

    more skeleton code

--- LaunchDay.ino
+++ LaunchDay.ino
@@ -1,21 +1,39 @@
-void setup() {
-  // put your setup code here, to run once:
-  int state = 0; //for launch day switch statement
+/****************************************************
+ * Phoenix College Acsend Launch Day Logic Fall 2015
+ * 
+ * Authors: 
+ ***************************************************/
 
-}
+void setup() {
+
+  int state = 0;     //for launch day switch statement
+  int t = 0;         //time keeper (miliseconds)
+  int initLoop = 1;  //initial loop indicator
+
+  Serial.begin(9600);
+
+  Serial.print("$ , , , , , , , , , , , , UV Level, UV Intensity (mW/cm^2) ");
+  Serial.print("\n");
+
+} //end setup()
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 
   //Launch Day State Machine
   switch(state) {
-    case: 0      //State 00, Waiting State
-      //some code here
+    case: 0      //State 00, Waiting State  
+      
+      if(t >= 120000) { state++; } //transision to next state
+      delay(1); //delay 1ms
+      t++; //keep time
+      
       break;
 
     case: 1      //State 01, Data Collection State
-      //some code here
+
+      //print data
+      
+      
       break;
-  }
-}
+  } //end state machine
+} //end loop()
