4fa884fd963dca8e7ccada68ee224c391e8f3f9a
jcahal <jacahal@gmail.com>
2015-11-13 11:47:53 -0700

    Barometer Inclusion

--- LaunchDay.ino
+++ LaunchDay.ino
@@ -12,7 +12,7 @@
 
 /////////////////////////////////////////////////////////
 //Includes, (these must be put into the libraries folder before coe will run)
-#include <Wire.h>              //IMU, Luminosity
+#include <Wire.h>              //IMU, Luminosity, Borometer, RGB
 
 #include <Adafruit_Sensor.h>   //IMU
 #include <Adafruit_LSM303_U.h> //IMU
@@ -21,6 +21,10 @@
 #include <Adafruit_10DOF.h>    //IMU
 
 #include <SparkFunTSL2561.h>   //Luminosity
+
+#include <SFE_BMP180.h>        //Barometer
+
+#include <SFE_ISL29125.h>      //RGB
 
 
 //IMU Definitions
@@ -43,6 +47,12 @@ unsigned int ms;  // Integration ("shutter") time in milliseconds
 //Hardware pin definitions
 int UVOUT = A0; //Output from the sensor
 int REF_3V3 = A1; //3.3V power on the Arduino board
+
+
+//Barometer Definitions
+/////////////////////////////////////////////////////////
+SFE_BMP180 pressure;
+#define ALTITUDE 1655.0 // Altitude of SparkFun's HQ in Boulder, CO. in meters
 
 
 
@@ -116,6 +126,20 @@ void setup(void){
   //////////////////////////////////////////////////////
   pinMode(UVOUT, INPUT);
   pinMode(REF_3V3, INPUT);
+
+  //BAROMETER SETUP
+  /////////////////////////////////////////////////////////
+  // Initialize the sensor (it is important to get calibration values stored on the device).
+  if (pressure.begin())
+    Serial.println("BMP180 init success");
+  else
+  {
+    // Oops, something went wrong, this is usually a connection problem,
+    // see the comments at the top of this sketch for the proper connections.
+
+    Serial.println("BMP180 init fail\n\n");
+    while(1); // Pause forever.
+  }
   
 
   //RGB SETUP
@@ -219,7 +243,76 @@ void loop() {
         
         //UV OPERATIONS
         //////////////////////////////////////////////////////
+
+
+        //BAROMETER OPERATIONS
+        /////////////////////////////////////////////////////////
+        char status;
+        double T,P,p0,a;
         
+       
+        status = pressure.startTemperature();
+        if (status != 0)
+        {
+          // Wait for the measurement to complete:
+          delay(status);
+      
+          // Retrieve the completed temperature measurement:
+          // Note that the measurement is stored in the variable T.
+          // Function returns 1 if successful, 0 if failure.
+      
+          status = pressure.getTemperature(T);
+          if (status != 0)
+          {
+            
+            // Start a pressure measurement:
+            // The parameter is the oversampling setting, from 0 to 3 (highest res, longest wait).
+            // If request is successful, the number of ms to wait is returned.
+            // If request is unsuccessful, 0 is returned.
+      
+            status = pressure.startPressure(3);
+            if (status != 0)
+            {
+              // Wait for the measurement to complete:
+              delay(status);
+      
+              // Retrieve the completed pressure measurement:
+              // Note that the measurement is stored in the variable P.
+              // Note also that the function requires the previous temperature measurement (T).
+              // (If temperature is stable, you can do one temperature measurement for a number of pressure measurements.)
+              // Function returns 1 if successful, 0 if failure.
+      
+              status = pressure.getPressure(P,T);
+              if (status != 0)
+              {
+      
+                // The pressure sensor returns abolute pressure, which varies with altitude.
+                // To remove the effects of altitude, use the sealevel function and your current altitude.
+                // This number is commonly used in weather reports.
+                // Parameters: P = absolute pressure in mb, ALTITUDE = current altitude in m.
+                // Result: p0 = sea-level compensated pressure in mb
+      
+                p0 = pressure.sealevel(P,ALTITUDE); // we're at 1655 meters (Boulder, CO)
+             
+                       // On the other hand, if you want to determine your altitude from the pressure reading,
+                // use the altitude function along with a baseline pressure (sea-level or other).
+                // Parameters: P = absolute pressure in mb, p0 = baseline pressure in mb.
+                // Result: a = altitude in m.
+      
+                a = pressure.altitude(P,p0);
+              }
+              else Serial.println("error retrieving pressure measurement\n");
+            }
+            else Serial.println("error starting pressure measurement\n");
+          }
+          else Serial.println("error with baro");
+        }
+        else Serial.println("error with baro\n");
+      
+        Serial.print(T,2);                      Serial.print(F(","));
+        Serial.print(P,2);                      Serial.print(F(","));
+        Serial.print(p0,2);                     Serial.print(F(","));
+
       
         //RGB OPERATIONS
         //////////////////////////////////////////////////////
